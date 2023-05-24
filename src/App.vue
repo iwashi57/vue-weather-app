ff93572ced66c494ed174cc06f990499443e791c
岩田峻 <shun15992@gmail.com>
2023-05-24 12:04:34 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <Title />
+      <Form @submit-form="getWeather" />
+      <Results :results="results" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+import axios from 'axios';
+import Title from './components/Title.vue';
+import Form from './components/Form.vue';
+import Results from './components/Results.vue';
+import './assets/base.css';
+const getWeather = (city) => {
+  axios
+    .get(
+      `http://api.weatherapi.com/v1/current.json?key=05051e5cbe434679aab04822232405&q=${city}&aqi=no`
+    )
+    .then((res) => {
+      (results.country = res.data.location.country),
+        (results.cityName = res.data.location.name),
+        (results.temperature = res.data.current.temp_c),
+        (results.conditionText = res.data.current.condition.text),
+        (results.icon = res.data.current.condition.icon);
+    });
+};
+const results = reactive({
+  country: '',
+  cityName: '',
+  temperature: '',
+  conditionText: '',
+  icon: '',
+});
+</script>
---
+++ src/components/Results.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>気象データ</h2>
+  <div v-if="results.country">{{ results.country }}</div>
+  <div v-if="results.cityName">{{ results.cityName }}</div>
+  <div v-if="results.temperature">{{ results.temperature }}</div>
+  <div v-if="results.icon">
+    <img :src="results.icon" alt="icon" />
+    <span>{{ results.conditionText }}</span>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  results: Object,
+});
+</script>
