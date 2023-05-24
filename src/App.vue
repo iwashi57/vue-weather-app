6d2f5ab73835bc0206a79306ada1be7f0cf332cb
岩田峻 <shun15992@gmail.com>
2023-05-24 13:55:38 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,23 @@
     <div class="container">
       <Title />
       <Form @submit-form="getWeather" />
-      <Results :results="results" />
+      <Results :results="results" v-if="!loading" />
+      <loading v-if="loading" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import axios from 'axios';
 import Title from './components/Title.vue';
 import Form from './components/Form.vue';
 import Results from './components/Results.vue';
 import './assets/base.css';
+import Loading from './components/Loading.vue';
+const loading = ref(false);
 const getWeather = (city) => {
+  loading.value = true;
   axios
     .get(
       `http://api.weatherapi.com/v1/current.json?key=05051e5cbe434679aab04822232405&q=${city}&aqi=no`
@@ -26,7 +30,9 @@ const getWeather = (city) => {
         (results.temperature = res.data.current.temp_c),
         (results.conditionText = res.data.current.condition.text),
         (results.icon = res.data.current.condition.icon);
-    });
+      loading.value = false;
+    })
+    .catch((err) => alert('error try plz'));
 };
 const results = reactive({
   country: '',
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,15 +1,20 @@
 <template>
   <h2>気象データ</h2>
-  <div v-if="results.country">{{ results.country }}</div>
-  <div v-if="results.cityName">{{ results.cityName }}</div>
-  <div v-if="results.temperature">{{ results.temperature }}</div>
+  <div v-if="results.country">{{ country }}</div>
+  <div v-if="results.cityName">{{ cityName }}</div>
+  <div v-if="results.temperature">{{ temperature }}</div>
   <div v-if="results.icon">
-    <img :src="results.icon" alt="icon" />
-    <span>{{ results.conditionText }}</span>
+    <img :src="icon" alt="icon" />
+    <span>{{ conditionText }}</span>
   </div>
 </template>
 <script setup>
+import { toRefs } from 'vue';
+
 const props = defineProps({
   results: Object,
 });
+const { country, cityName, temperature, icon, conditionText } = toRefs(
+  props.results
+);
 </script>
